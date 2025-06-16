129afc1c97a851dc20b1e112bc03bd81e17a6193
August <147424439+August-WW@users.noreply.github.com>
2025-06-16 16:30:36 -0400

    Student.cpp

---
+++ Labs/LAB 11.13/Student.cpp
@@ -0,0 +1,37 @@
+// Simple class to hold student information
+#include "Student.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+ 
+Student::Student() {
+   name = "Louie";
+   id = "000000";
+   gpa = 0.0;
+}
+
+void Student::SetName(string n) {
+	name = n;
+}
+
+string Student::GetName() {
+    return name;
+}
+
+void Student::SetID(string newID) {
+    id = newID;
+}
+
+string Student::GetID() {
+    return id;
+}
+
+void Student::SetGPA(double newGPA) {
+    gpa = newGPA;
+}
+
+double Student::GetGPA() {
+    return gpa;
+}
