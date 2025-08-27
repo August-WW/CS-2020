22deb309dc1568ade73c3f1cebaed3a5ce3fd4a1
August <147424439+August-WW@users.noreply.github.com>
2025-08-26 22:38:41 -0400

    Course

---
+++ Labs/LAB 15.11/OfferedCourse.h
@@ -0,0 +1,26 @@
+#ifndef OFFERED_COURSEH
+#define OFFERED_COURSEH
+
+#include "Course.h"
+
+class OfferedCourse : public Course {
+	// TODO: Declare private data members
+private:
+    string instructorName;
+    string location;
+    string classTime;
+
+public:
+    void SetInstructorName(string name);
+    void SetLocation(string loc);
+    void SetClassTime(string time);
+
+    string GetInstructorName() const;
+    string GetLocation() const;
+    string GetClassTime() const;
+
+    void PrintInfo() const;
+
+};
+
+#endif
